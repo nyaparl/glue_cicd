5da67f5d3430e0f6f2bff16c0b5d311a4bb4cac8
nyaparl <161057479+nyaparl@users.noreply.github.com>
2024-05-29 12:13:01 +0530

    Update glue_script.py

--- scripts/glue_script.py
+++ scripts/glue_script.py
@@ -3,8 +3,8 @@ from sessionCreation import *
 # Spark session created.
 spark = sparkSession()
 
-input_path = "s3://mygluecicd-20240528-01/input/OfficeDataProject.csv"
+input_path = "s3://mybucket-20240529-new/input/sample.csv"
 df = spark.read.format('csv').option('header','ture').load(input_path)
 
-output_path = "s3://mygluecicd-20240528-01/input/output.json"
+output_path = "s3://mybucket-20240529-new/input/sample.json"
 df.coalesce(1).write.format('json').mode('overwrite').save(output_path)
