3bf61479658647b78fc322356f4723844b1ff06b
nyaparl <161057479+nyaparl@users.noreply.github.com>
2024-05-29 13:39:45 +0530

    Update glue_script.py

--- scripts/glue_script.py
+++ scripts/glue_script.py
@@ -9,7 +9,7 @@ from pyspark.sql.types import *
 spark = SparkSession.builder.appName('first').master('local[*]').getOrCreate()
 
 input_path = "s3://mybucket-20240529-new/input/sample.csv"
-df = spark.read.format('csv').option('header','ture').load(input_path)
+df = spark.read.format('csv').option('header','true').load(input_path)
 
 output_path = "s3://mybucket-20240529-new/input/sample.json"
 df.coalesce(1).write.format('json').mode('overwrite').save(output_path)
